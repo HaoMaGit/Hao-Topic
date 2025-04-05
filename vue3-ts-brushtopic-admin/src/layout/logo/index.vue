305d49967f5f018a7670fe6e2f88c0697721ae10
易雨豪 <>
2025-04-05 15:05:18 +0800

    前端-封装LOGO组件

---
+++ vue3-ts-brushtopic-admin/src/layout/logo/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { useSettingStore } from '@/stores/modules/setting.js';
+// 实例化设置仓库
+const settingStore = useSettingStore();
+
+</script>
+<template>
+  <div class="logo" :style="{ 'marginLeft': settingStore.fold ? '2px' : '0px' }">
+    <img src="/public/logo.svg" alt="">
+    <p :class="{ 'text-hidden': settingStore.fold, 'text-preview': !settingStore.fold }">易题后台管理系统</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.logo {
+  width: 100%;
+  height: $base-menu-logo-height;
+  color: white;
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  transition: all 0.3s ease;
+
+  img {
+    width: 30px;
+    height: 30px;
+  }
+
+  p {
+    font-size: 15px;
+    margin-left: 6px;
+  }
+
+  .text-hidden {
+    opacity: 0;
+  }
+
+  .text-preview {
+    white-space: nowrap;
+    overflow: hidden;
+  }
+}
+</style>
