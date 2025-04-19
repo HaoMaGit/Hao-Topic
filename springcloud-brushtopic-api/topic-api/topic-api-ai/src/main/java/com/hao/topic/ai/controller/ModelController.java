5b5468e29325646ceb29c46db65b144a3875490f
易雨豪 <3655161743@qq.com>
2025-04-20 00:01:21 +0800

    后端-集成springAi封装ollama客户端配置类

--- springcloud-brushtopic-api/topic-api/topic-api-ai/src/main/java/com/hao/topic/ai/controller/ModelController.java
+++ springcloud-brushtopic-api/topic-api/topic-api-ai/src/main/java/com/hao/topic/ai/controller/ModelController.java
@@ -1,7 +1,12 @@
 package com.hao.topic.ai.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * Description: Ai模型控制器
@@ -10,5 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("ai/model")
+@AllArgsConstructor
 public class ModelController {
+    private final ChatClient chatClient;
+
+    /**
+     * 流式对话
+     *
+     * @param prompt
+     * @return
+     */
+    @GetMapping("/chat")
+    public Flux<String> chat() {
+        try {
+            return chatClient
+                    .prompt()
+                    .user("你是谁？")
+                    .stream()
+                    .content();
+        } catch (Exception e) {
+            return Flux.just("出错了");
+        }
+    }
 }
