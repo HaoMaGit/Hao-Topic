aaf8f2e7bcb87182e68477b5407203b430e47bfa
易雨豪 <3655161743@qq.com>
2025-04-26 21:52:07 +0800

    后端-使用redis添加分布式锁防止审核消息重复消费

--- springcloud-brushtopic-api/topic-api/topic-api-ai/src/main/java/com/hao/topic/ai/receiver/AuditReceiver.java
+++ springcloud-brushtopic-api/topic-api/topic-api-ai/src/main/java/com/hao/topic/ai/receiver/AuditReceiver.java
@@ -1,8 +1,10 @@
 package com.hao.topic.ai.receiver;
 
+import com.alibaba.excel.util.StringUtils;
 import com.alibaba.fastjson2.JSON;
 import com.hao.topic.ai.service.ModelService;
 import com.hao.topic.api.utils.constant.RabbitConstant;
+import com.hao.topic.common.constant.RedisConstant;
 import com.hao.topic.model.dto.audit.TopicAudit;
 import com.hao.topic.model.dto.audit.TopicAuditCategory;
 import com.hao.topic.model.dto.audit.TopicAuditLabel;
@@ -15,7 +17,10 @@ import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Description: 接收专题审核消息
@@ -28,6 +33,8 @@ public class AuditReceiver {
 
     @Autowired
     private ModelService modelService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
 
     /**
@@ -43,12 +50,21 @@ public class AuditReceiver {
                     key = {RabbitConstant.CATEGORY_AUDIT_ROUTING_KEY_NAME}))// 路由key
     public void auditCategory(String topicAuditCategoryJson, Message message, Channel channel) {
         log.info("接收到分类审核消息{}", topicAuditCategoryJson);
-        // TODO 将消息存入redis判断是否有
-        // 如果有就直接返回
-        // 没有就消费
         // 转换json
         TopicAuditCategory topicAuditCategory = JSON.parseObject(topicAuditCategoryJson, TopicAuditCategory.class);
+        // 锁的key
+        String lockKey = RedisConstant.CATEGORY_AUDIT_KEY_PREFIX + topicAuditCategory.getId();
+        // 锁的value
+        String lockValue = String.valueOf(topicAuditCategory.getId());
         try {
+            // 尝试获取锁,并设置过期时间
+            Boolean acquiredLock = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, lockValue, RedisConstant.AUDIT_EXPIRE_TIME, TimeUnit.MINUTES);
+            if (acquiredLock == null || !acquiredLock) {
+                log.info("分类审核消息正在处理中，忽略重复消息: {}", topicAuditCategory.getId());
+                // 确认消息，避免重复消费
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+                return;
+            }
             // 开始审核
             modelService.auditCategory(topicAuditCategory);
             // 手动确认该消息 通过唯一标识已被消费
@@ -56,8 +72,9 @@ public class AuditReceiver {
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         } catch (Exception e) {
             modelService.recordAuditLog("服务器发生异常", topicAuditCategory.getAccount(), topicAuditCategory.getUserId());
+        } finally {
+            stringRedisTemplate.delete(lockKey);
         }
-
     }
 
     /**
@@ -73,12 +90,21 @@ public class AuditReceiver {
                     key = {RabbitConstant.SUBJECT_AUDIT_ROUTING_KEY_NAME}))// 路由key
     public void auditSubject(String topicAuditSubjectJson, Message message, Channel channel) {
         log.info("接收到专题审核消息{}", topicAuditSubjectJson);
-        // TODO 将消息存入redis判断是否有
-        // 如果有就直接返回
-        // 没有就消费
         // 转换json
         TopicAuditSubject topicAuditSubject = JSON.parseObject(topicAuditSubjectJson, TopicAuditSubject.class);
+        // 锁的key
+        String lockKey = RedisConstant.SUBJECT_AUDIT_KEY_PREFIX + topicAuditSubject.getId();
+        // 锁的value
+        String lockValue = String.valueOf(topicAuditSubject.getId());
         try {
+            // 尝试获取锁,并设置过期时间
+            Boolean acquiredLock = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, lockValue, RedisConstant.AUDIT_EXPIRE_TIME, TimeUnit.MINUTES);
+            if (acquiredLock == null || !acquiredLock) {
+                log.info("审核消息正在处理中，忽略重复消息: {}", topicAuditSubject.getId());
+                // 确认消息，避免重复消费
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+                return;
+            }
             // 开始审核
             modelService.auditSubject(topicAuditSubject);
             // 手动确认该消息 通过唯一标识已被消费
@@ -86,6 +112,8 @@ public class AuditReceiver {
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         } catch (Exception e) {
             modelService.recordAuditLog("服务器发生异常", topicAuditSubject.getAccount(), topicAuditSubject.getUserId());
+        } finally {
+            stringRedisTemplate.delete(lockKey);
         }
     }
 
@@ -102,12 +130,21 @@ public class AuditReceiver {
                     key = {RabbitConstant.LABEL_AUDIT_ROUTING_KEY_NAME}))// 路由key
     public void auditLabel(String topicAuditLabelJson, Message message, Channel channel) {
         log.info("接收到标签审核消息{}", topicAuditLabelJson);
-        // TODO 将消息存入redis判断是否有
-        // 如果有就直接返回
-        // 没有就消费
         // 转换json
         TopicAuditLabel topicAuditLabel = JSON.parseObject(topicAuditLabelJson, TopicAuditLabel.class);
+        // 锁的key
+        String lockKey = RedisConstant.LABEL_AUDIT_KEY_PREFIX + topicAuditLabel.getId();
+        // 锁的value
+        String lockValue = String.valueOf(topicAuditLabel.getId());
         try {
+            // 尝试获取锁,并设置过期时间
+            Boolean acquiredLock = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, lockValue, RedisConstant.AUDIT_EXPIRE_TIME, TimeUnit.MINUTES);
+            if (acquiredLock == null || !acquiredLock) {
+                log.info("审核消息正在处理中，忽略重复消息: {}", topicAuditLabel.getId());
+                // 确认消息，避免重复消费
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+                return;
+            }
             // 开始审核
             modelService.auditLabel(topicAuditLabel);
             // 手动确认该消息 通过唯一标识已被消费
@@ -115,6 +152,8 @@ public class AuditReceiver {
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
         } catch (Exception e) {
             modelService.recordAuditLog("服务器发生异常", topicAuditLabel.getAccount(), topicAuditLabel.getUserId());
+        } finally {
+            stringRedisTemplate.delete(lockKey);
         }
     }
 
@@ -131,12 +170,22 @@ public class AuditReceiver {
                     key = {RabbitConstant.TOPIC_AUDIT_ROUTING_KEY_NAME}))// 路由key
     public void auditTopic(String topicAuditJson, Message message, Channel channel) {
         log.info("接收到题目审核消息{}", topicAuditJson);
-        // TODO 将消息存入redis判断是否有
-        // 如果有就直接返回
-        // 没有就消费
+
         // 转换json
         TopicAudit topicAudit = JSON.parseObject(topicAuditJson, TopicAudit.class);
+        // 锁的key
+        String lockKey = RedisConstant.TOPIC_AUDIT_KEY_PREFIX + topicAudit.getId();
+        // 锁的value
+        String lockValue = String.valueOf(topicAudit.getId());
         try {
+            // 尝试获取锁,并设置过期时间
+            Boolean acquiredLock = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, lockValue, RedisConstant.AUDIT_EXPIRE_TIME, TimeUnit.MINUTES);
+            if (acquiredLock == null || !acquiredLock) {
+                log.info("审核消息正在处理中，忽略重复消息: {}", topicAudit.getId());
+                // 确认消息，避免重复消费
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+                return;
+            }
             // 开始审核
             modelService.auditTopic(topicAudit);
             // 手动确认该消息 通过唯一标识已被消费
@@ -160,9 +209,6 @@ public class AuditReceiver {
                     key = {RabbitConstant.AI_ANSWER_ROUTING_KEY_NAME}))// 路由key
     public void generateAnswer(String topicAuditJson, Message message, Channel channel) {
         log.info("接收到题目生成答案消息{}", topicAuditJson);
-        // TODO 将消息存入redis判断是否有
-        // 如果有就直接返回
-        // 没有就消费
         // 转换json
         TopicAudit topicAudit = JSON.parseObject(topicAuditJson, TopicAudit.class);
         try {
