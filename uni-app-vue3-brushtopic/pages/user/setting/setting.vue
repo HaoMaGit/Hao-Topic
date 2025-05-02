d8e1d768c7b987bd489d1013dabc05cfa19a2209
易雨豪 <3655161743@qq.com>
2025-05-02 12:22:23 +0800

    h5-重构设置页面布局和样式

--- uni-app-vue3-brushtopic/pages/user/setting/setting.vue
+++ uni-app-vue3-brushtopic/pages/user/setting/setting.vue
@@ -1,178 +1,179 @@
 <script setup>
-	import {
-		ref
-	} from 'vue'
-	// 头像的样式
-	const imageStyles = ref({
-		'width': 30,
-		'height': 30,
-		"border": false, // 是否显示边框
+import { ref } from 'vue'
+
+// 头像的样式
+const imageStyles = ref({
+	width: 80,
+	height: 80,
+	border: false,
+})
+
+// 图片上传成功的回调
+const uploadSuccess = (e) => {
+	console.log('上传成功', e)
+}
+
+// 修改名称对话框
+const nameDialog = ref()
+
+// 跳转修改密码
+const goToChange = () => {
+	uni.navigateTo({
+		url: '/pages/change/change'
 	})
+}
 
-	// 图片上传成功的回调
-	const uploadSuccess = (e) => {
-		console.log(e);
-		console.log('上传成功');
-	}
+// 退出登录
+const logout = () => {
+	uni.showModal({
+		title: '提示',
+		content: '确定要退出登录吗？',
+		success: (res) => {
+			if (res.confirm) {
+				// 执行退出登录逻辑
+				console.log('用户点击确定')
+			}
+		}
+	})
+}
 
-	// 修改名称对话框
-	const nameDialog = ref()
-
-	// 跳转修改密码
-	const goToChange = () => {
-		uni.navigateTo({
-			url: '/pages/change/change'
-		})
-	}
-
-	// 清除数据
-	const alertDialog = ref()
+// 开启自定义题目
+const isCustomQuestion = ref(false)
 </script>
 <template>
 	<view class="setting-box">
-		<!-- 清除数据的弹层 -->
-		<uni-popup ref="alertDialog" type="dialog">
-			<uni-popup-dialog type="error" cancelText="取消" confirmText="清除" title="清除数据"
-				content="确定要清除刷题记录以及所有的数据"></uni-popup-dialog>
-		</uni-popup>
 		<!-- 修改名称的弹层 -->
 		<uni-popup ref="nameDialog" type="dialog">
-			<uni-popup-dialog ref="inputClose" mode="input" title="修改名称" value="对话框预置提示内容!" placeholder="AI用户"
+			<uni-popup-dialog ref="inputClose" mode="input" title="修改名称" placeholder="请输入新的名称"
 				@confirm="dialogInputConfirm"></uni-popup-dialog>
 		</uni-popup>
+
 		<!-- 操作列表 -->
 		<view class="section">
-			<!-- 外层盒子 -->
 			<view class="list">
-				<!-- 跳转链接 -->
-				<!-- 每一行 -->
 				<view class="row">
 					<view class="left">
-						<view class="text">
-							修改头像
-						</view>
+						<text class="label">修改头像</text>
 					</view>
 					<view class="right">
 						<uni-file-picker @select="uploadSuccess" limit="1" :del-icon="false" disable-preview
 							:imageStyles="imageStyles" file-mediatype="image">
-							<uni-icons type="right" size="22" color="#a6a6a6"></uni-icons>
+							<uv-avatar class="avatar" size="55" src="https://via.placeholder.com/200x200.png/2878ff"></uv-avatar>
 						</uni-file-picker>
 					</view>
 				</view>
 
 				<view class="row">
 					<view class="left">
-						<view class="text">
-							修改名称
-						</view>
+						<text class="label">修改昵称</text>
 					</view>
 					<view class="right" @click="nameDialog.open()">
-						<uni-icons type="right" size="22" color="#a6a6a6"></uni-icons>
+						<text class="value">书中易语</text>
+						<uni-icons type="right" size="16" color="#999"></uni-icons>
 					</view>
 				</view>
 
 				<view class="row">
 					<view class="left">
-						<view class="text">
-							修改密码
-						</view>
+						<text class="label">修改密码</text>
 					</view>
 					<view class="right" @click="goToChange">
-						<uni-icons type="right" size="22" color="#a6a6a6"></uni-icons>
+						<uni-icons type="right" size="16" color="#999"></uni-icons>
 					</view>
 				</view>
+
+				<!-- 加一个开启展示自定义题目需要身份为identify1才展示 -->
 				<view class="row">
 					<view class="left">
-						<view class="text">
-							清除数据
-						</view>
+						<text class="label">会员展示题目</text>
 					</view>
-					<view class="right" @click="alertDialog.open()">
-						<uni-icons type="right" size="22" color="#a6a6a6"></uni-icons>
+					<view class="right">
+						<uv-switch v-model="isCustomQuestion" activeColor="#1677ff"></uv-switch>
 					</view>
 				</view>
 			</view>
-			<!-- 按钮的占位符 -->
-			<view class="btn-box">
 
+			<!-- 退出登录按钮 -->
+			<view class="logout-btn">
+				<button class="btn" hover-class="btn-hover" @click="logout">退出登录</button>
 			</view>
-			<!-- 退出登录 -->
-			<uv-button :plain="true">
-				<template v-slot:default>
-					<text style="color: black;">退出登录</text>
-				</template>
-			</uv-button>
 		</view>
 	</view>
-
 </template>
+
 <style lang="scss" scoped>
-	.section {
-		width: 690rpx;
-		margin: 10rpx auto;
-		border: 1px solid #eeeeee;
-		border-radius: 10rpx;
-		box-shadow: 0 0 30rpx rgba(0, 0, 0, 0.05);
+// 定义主题色变量
+$theme-color: #1677ff;
 
-		.pdContent {
-			width: 200rpx;
-			height: 400rpx;
-			background-color: pink;
-		}
+.setting-box {
+	min-height: 100vh;
+	background-color: #f5f5f5;
+	padding: 15rpx;
+	padding-top: 0;
+}
 
-		.btn-box {
-			height: 10rpx;
-			background-color: #eff0f3;
-		}
+.section {
+	background: #fff;
+	border-radius: 18rpx;
+	padding: 0 20rpx;
+	margin: 20rpx auto;
+	box-shadow: 0 2rpx 12rpx rgba(0, 0, 0, 0.03);
 
-		.list {
-			.row {
-				background-color: #fff;
+	.list {
+		.row {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 30rpx 0;
+			border-bottom: 1px solid #f5f5f5;
+
+
+			.left {
+				.label {
+					font-size: 28rpx;
+					color: #333;
+					font-weight: 500;
+				}
+			}
+
+			.right {
 				display: flex;
 				align-items: center;
-				justify-content: space-between;
-				height: 95rpx;
-				padding: 0 20rpx;
-				position: relative;
+				gap: 10rpx;
 
-				&:last-child {
-					border-bottom: 0;
+				.avatar {
+					padding-right: 15rpx;
 				}
 
-				.left {
-					display: flex;
-					align-items: center;
-
-					.text {
-						padding-left: 20rpx;
-						color: #5b5b5b;
-					}
-
-					.icon {
-						color: #28b28b;
-					}
-				}
-
-				.right {
-					display: flex;
-					align-items: center;
-
-					.text {
-						font-size: 28rpx;
-						color: #aaa;
-						padding-right: 5rpx;
-					}
-				}
-
-				button {
-					position: absolute;
-					top: 0;
-					left: 0;
-					height: 100rpx;
-					width: 100%;
-					opacity: 0;
+				.value {
+					font-size: 28rpx;
+					color: #999;
+					margin-right: 10rpx;
 				}
 			}
 		}
 	}
+}
+
+.logout-btn {
+	padding-bottom: 20rpx;
+	margin: 60rpx 20rpx;
+
+	.btn {
+		width: 100%;
+		height: 88rpx;
+		line-height: 88rpx;
+		text-align: center;
+		background: #fff;
+		color: $theme-color;
+		font-size: 32rpx;
+		border-radius: 44rpx;
+		border: 1px solid $theme-color;
+		transition: all 0.3;
+
+		&.btn-hover {
+			opacity: 0.8;
+		}
+	}
+}
 </style>
